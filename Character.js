74f2de2901594d1cb43a45fb54bfdddaa6ad3e25
Mariam Oluseyi-Alli <81536650+mariamALLI@users.noreply.github.com>
2023-03-19 22:27:56 +0100

    wire the btn to help initalize the game

--- Character.js
+++ Character.js
@@ -1,4 +1,4 @@
-import {getDiceRollArray} from '/utils.js'
+import {getDiceRollArray, getDicePlaceholderHtml} from './utils.js'
 
 /*
 The character() carries out the constructor function with the object.assgin which helps make a copy of the 
@@ -7,16 +7,25 @@ string for the game avater design and diceroll.
 */ 
 function Character(data) {
     Object.assign(this, data)
+
+    this.diceArray = getDicePlaceholderHtml(this.diceCount)
     
-    this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num){ 
-            return  `<div class="dice">${num}</div>`
-        }).join('')
+    /* this.getDiceHtml = function(diceCount) {
+    //     return getDiceRollArray(diceCount).map(function(num){ 
+    //         return  `<div class="dice">${num}</div>`
+    //     }).join('')
+    // }this code was replaced by the diceArray holding the getDicePlaceholderHtml() which houses the 
+    empty display box at the start of the game rather than already decleared number template
+    string
+    */
+
+    this.getDiceHtml = function(){
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
     }
 
     this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;      
-        let diceHtml = this.getDiceHtml(diceCount);
+        const { elementId, name, avatar, health, diceCount, diceArray } = this;      
+        // let diceHtml = this.getDiceHtml(diceCount);
         
            return `
             <div class="character-card">
@@ -24,7 +33,7 @@ function Character(data) {
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
                 <div class="dice-container">
-                    ${diceHtml}
+                    ${diceArray}
                 </div>
             </div>`;
     }  
--- index.js
+++ index.js
@@ -2,13 +2,20 @@ import characterData from '/data.js'
 import Character from '/Character.js'
 
 /*
-the render() function below helps display the game onto the DOM
+- the render() function below helps display the game onto the DOM
+- the attack() function helps involkes the render() by involking the attack() inside the
+eventlistener of the attack btn.
 */ 
+function attack(){
+     render()
+}
+
 function render() {
     document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
     document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
 
+
+document.getElementById('attack-button').addEventListener('click', attack)
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
-render()
